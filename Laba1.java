f3a1add2240a3a608160561b27b684b33ffa1e5e
maks-s0nchik <maksimpron3@gmail.com>
2025-02-07 18:52:33 +0200

    Laba1.java

--- Laba1.java
+++ Laba1.java
@@ -1,7 +1,10 @@
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Laba1 {
     public static void main(String[] args) {
+        System.setOut(new PrintStream(System.out, true, StandardCharsets.UTF_8));
          try (Scanner scanner = new Scanner(System.in)) {
             System.out.print("Введіть своє ім'я: ");
             String name = scanner.nextLine();
