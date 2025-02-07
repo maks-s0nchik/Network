a96c897a445a3ad6290186b30f9079e09d5d2dcc
maks-s0nchik <maksimpron3@gmail.com>
2025-02-07 19:31:30 +0200

    Laba1.java

--- Laba1.java
+++ Laba1.java
@@ -1,19 +1,16 @@
-import java.io.PrintStream;
-import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Laba1 {
-    public static void main(String[] args) {
-        System.setOut(new PrintStream(System.out, true, StandardCharsets.UTF_8));
-         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Введіть своє ім'я: ");
-            String name = scanner.nextLine();
-            System.out.println("Привіт, " + name + "!");
-            System.out.print("Скільки вам років? ");
-            int age = scanner.nextInt();
-            int currentYear = java.time.Year.now().getValue();
-            int birthYear = currentYear - age;
-            System.out.println("Ви народилися у " + birthYear + " році.");
-        }
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Як вас звати? ");
+        String name = reader.readLine();
+        System.out.println("Привіт, " + name + "!");
+        int now = 2025;
+        System.out.print("Скільки вам років? ");
+        int age = Integer.parseInt(reader.readLine());
+        System.out.println("Ви народилися у " + (now - age) + " році.");
     }
 }
