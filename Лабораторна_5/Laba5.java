de34e8667cb4bc9130337d636c8b4512a3f0cc64
maks-s0nchik <maksimpron3@gmail.com>
2025-04-03 11:33:01 +0300

    Додано файл

---
+++ Лабораторна_5/Laba5.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+
+public class Laba5 {
+    public static void main(String[] args) {
+        int[][] N = new int[4][5];
+        int[] products = new int[5];
+        Random rand = new Random();
+
+        // Заповнення масиву та одночасний вивід
+        System.out.println("Двовимірний масив N:");
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                N[i][j] = rand.nextInt(100); // Числа від 0 до 99
+                System.out.print(N[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        // Обчислення добутків
+        for (int j = 0; j < 5; j++) {
+            products[j] = 1;
+            boolean hasValid = false;
+            for (int i = 0; i < 4; i++) {
+                if (N[i][j] >= 10 && N[i][j] < 55) {
+                    products[j] *= N[i][j];
+                    hasValid = true;
+                }
+            }
+            if (!hasValid) products[j] = 0;
+        }
+
+        // Вивід результату
+        System.out.println("Добутки значень елементів кожного стовпця (де елементи у [10;55)):");
+        for (int product : products) {
+            System.out.print(product + "\t");
+        }
+    }
+}
